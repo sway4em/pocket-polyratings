72eadc0a5fbb2669194514150f70e58f85e6de93
Swayam Chidrawar <schidraw@calpoly.edu>
2023-06-27 23:28:02 -0700

    WORKING!!!

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (tab.url === 'https://cmsweb.pscs.calpoly.edu/') {
+        chrome.tabs.executeScript(tab.id, {
+            code: `
+                const iframe = document.querySelector('iframe');
+                const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+                const divs = iframeDocument.querySelectorAll(".cx-MuiTypography-root.cx-MuiTypography-body2.cx-MuiTypography-noWrap");
+
+                for (let i = 0; i < divs.length; i += 5) {
+                    if (divs[i].getAttribute("role") === "cell") {
+                        const cellText = divs[i].textContent;
+                        console.log(cellText);
+                    }
+                }
+            `
+        });
+    }
+});
