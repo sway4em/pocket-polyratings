6dd42bcebe575ce17ef0cda4dd27296d4689965d
Swayam Chidrawar <schidraw@calpoly.edu>
2023-06-28 01:41:51 -0700

    Remove script2.js

--- script2.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status == "complete") {
-    var element = document.querySelector("div[aria-label=\"Sections List\"]");
-    console.log(element.textContent);
-  }
-});
